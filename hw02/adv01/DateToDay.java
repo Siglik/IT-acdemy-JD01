7fb533bbda984a025916c7c58eebb7eee9cab5ec
qqq175 <7078009@gmail.com>
2016-07-08 04:01:57 +0300

    finished hw2

---
+++ hw02/adv01/DateToDay.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author qqq175
+ *
+ */
+public class DateToDay {
+	private static String[] daysOfWeek = {"Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"};
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Введите год: ");
+		int year = input.nextInt();
+		System.out.print("Введите месяц (1-12): ");
+		int month = input.nextInt();
+		if (month < 1 || month > 12)
+		{
+			System.out.println("Введено неверное значение: ");
+			return;
+		}
+		System.out.println("Введите день в диапазоне от 1 до "+ daysInMonth(year, month) +": ");
+		int day = input.nextInt();
+		if (day < 1 || day > daysInMonth(year, month))
+		{
+			System.out.println("Введено неверное значение: ");
+			return;
+		}
+		input.close();
+		
+		 //change modern calendar to calendar that is begin from march
+		int ages = year / 100;
+		year = year % 100;
+		month = (month + 9) % 12 + 1;  
+		
+		int dayNumber = (day + ((13*month -1)/5) + year +  (year/4) +  (ages/4) -2*ages +777) % 7 ;
+		
+		System.out.println(dayOfWeek(dayNumber));
+	}
+/**
+ * Returns how many days in a month
+ * @param year
+ * @param month
+ * @return day amount in month, or 0 if wrong year or month input
+ */
+private static int daysInMonth(int year, int month) {
+		switch(month){
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return 31;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return 30;
+		case 2:
+			if(isYearLeap(year))
+			{
+				return 29;
+			} else {
+				return 28;
+			}
+		default:
+			return 0;
+		}
+}
+
+	/**
+	 * Check whether year is a leap year.
+	 * @param year
+	 * @return
+	 */
+private static boolean isYearLeap(int year)
+{
+	boolean isLeap = false;
+	
+	if (year % 4 == 0){
+		if (year % 100 == 0){
+			if (year % 400 == 0){
+				isLeap = true;
+			} else {
+				isLeap = false;
+			}
+		} else {
+			isLeap = true;
+		}
+	} else{
+		isLeap = false;
+	}
+	return isLeap;
+}
+
+/**
+ * Return a weekday name equivalent to number of the day.
+ * @param dayNumber is number from 1 to 7
+ * @return a weekday name string or empty string if dayNumber not in range from 1 to 7
+ */
+	private static String dayOfWeek(int dayNumber) {
+		if (0 <= dayNumber && dayNumber <= 6) {
+			return daysOfWeek[dayNumber];
+		} else {
+			return "";
+		}
+	}
+
+}
---
+++ hw02/adv07/Calculator.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+/**
+ * Console menu calculator. Can divide and add numbers.
+ * @author qqq175
+ *
+ */
+public class Calculator {
+
+	static Scanner input; //Scanner object
+	/**
+	 * main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Здравствуйте, я Консольный калькулятор 1.0. \nЯ умею делить и складывать числа.");
+		
+		input = new Scanner(System.in);
+		mainMenu();
+		input.close();
+		
+		System.out.println("До свидания!");
+	}
+
+/**
+ * method that implements menu logic
+ */
+static void mainMenu()
+{
+	double[] operand = new double[2];  //operators
+	int	operationId = 0;		//operationId for calculate steps
+	char operation = '\u0000';  //char to contain selected operation
+	String in = "";    //string to save user input
+	
+	while(operationId != -1)  //-1 for exit
+	{
+		switch (operationId)
+		{
+		case 0:  //input operator 1
+			in = inputNumberOrQ(1);
+			if (!in.equalsIgnoreCase("q")){
+				operand[0] = Double.parseDouble(in);
+				operationId++;
+			} else {  //exit ('q' entered)
+				operationId = -1;
+			}
+			break;
+		case 1:  //choose operation
+			in = inputOperatorOrQ();
+			if (!in.equalsIgnoreCase("q")){
+				operation = in.charAt(0);
+				operationId++;
+			} else { //exit ('q' entered)
+				operationId = -1;
+			}
+			break;
+		case 2:   //input operator 2
+			in = inputNumberOrQ(2);
+			if (!in.equalsIgnoreCase("q")){
+				operand[1] = Double.parseDouble(in);
+				operationId++;
+			} else {  //exit ('q' entered)
+				operationId = -1;
+			}
+			break;
+		case 3:  //calculate and print result
+			switch (operation)
+			{
+			case '+':
+				System.out.println(operand[0] + " + " + operand[1] + " = " + (operand[0]+operand[1]));
+				break;
+			case '/':
+				if (operand[1] != 0)
+				{
+					System.out.println(operand[0] + " / " + operand[1] + " = " + (operand[0]/operand[1]));
+				} else {
+					System.out.println("Ошибка деления при делении на 0.");
+				}
+				break;
+			default:
+				System.out.println("Неопознанная ошибка.");
+			}
+			operationId = 0;  //start again
+			break;
+		default:
+			System.out.println("Неопознанная ошибка.");
+		}
+		System.out.println();
+	}
+}
+
+/**
+ * Ask user to input correct number or 'q', check it and repeat while input data is incorrect
+ * @param operandId
+ * @return String contains 'q', 'Q' оr correct number/
+ */
+static String inputNumberOrQ(int operandId)
+{
+	String result = "";
+	while (true) //repeat until input is correct
+	{
+		System.out.print("Введите операнд "  + operandId + " и нажмите <Enter> (введите 'q' для выхода): ");
+		result = input.next().trim();
+		if (result.equalsIgnoreCase("q"))  //is input a quit command
+		{
+			break;   //break and return input
+		}
+		if (isNumber(result))  //is input a number
+		{
+			break;   //break and return input
+		}
+		System.out.println("Неверный формат числа!");
+	}
+	
+	return result;
+}
+
+/**
+ *  Ask user to input operator or 'q', check it and repeat while input data is incorrect
+ * @return String contains "q", "Q",  "/" or "+".
+ */
+static String inputOperatorOrQ()
+{
+	String result = "";
+	while (true)  //repeat until input is correct
+	{
+		System.out.println("Выберите оператор:\n\t\'/' - разделить\n\t'+' - прибавить");
+		System.out.print("Введите оператор (введите 'q' для выхода): ");
+		result = input.next().trim();
+		if (result.equals("+") || result.equals("/") ||result.equalsIgnoreCase("q"))  //check input string
+		{
+			break; 		  //if is OK, break cycle and return
+		}
+		System.out.println("Не могу распознать оператор!");
+	}
+	return result;
+}
+
+/**
+ * Check if string can be parsed to Double (is correct number string)
+ * @param string
+ * @return True if string can be parsed to Double.
+ */
+static boolean isNumber(String string)
+{
+	boolean result = true;
+	
+	try {
+		Double.parseDouble(string);  //try to parse
+	}
+	catch (NumberFormatException e){
+		result = false;  //if exception - string isn't number - return false
+	}
+	
+	return result;
+}
+}
